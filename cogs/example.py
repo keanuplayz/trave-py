c926b48dc099008fbfaca104d28e500991715b6b
Keanu <keanutimmermans8@gmail.com>
2020-04-13 15:19:04 +0200

    Fixed example cog.

--- cogs/example.py
+++ cogs/example.py
@@ -1,24 +1,24 @@
 # Basic imports:
-# import discord
-# from discord.ext import commands
+import discord
+from discord.ext import commands
 
 # Cog class:
-# class Example(commands.Cog):
+class Example(commands.Cog):
 
-#     Forgot what this does, add it:
-#     def __init__(self, client):
-#         self.client = client
+#   Forgot what this does, add it:
+    def __init__(self, client):
+        self.client = client
 
-#     This is an event:
-#     @commands.Cog.listener()
-#     async def on_ready(self):
-#         print('This will be printed to the console.')
+#   This is an event:
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('This will be printed to the console.')
 
-#     This is a command:
-#     @commands.command()
-#     async def ping(self, ctx):
-#         await ctx.send("pong nigga")
+#   This is a command:
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send("pong nigga")
 
 # This always needs to be at the end of a cog file:
-# def setup(client):
-#     client.add_cog(Example(client))
+def setup(client):
+    client.add_cog(Example(client))
